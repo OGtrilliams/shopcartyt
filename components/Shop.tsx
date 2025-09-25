6e4b49523c8596626bb15851738fa3c96ebb16eb
OGtrilliams <tribecca@tribecc.us>
2025-09-25 08:27:59 -0400

    product categories section

--- components/Shop.tsx
+++ components/Shop.tsx
@@ -1,16 +1,27 @@
 "use client";
-import { BRANDS_QUERYResult, Category } from "@/sanity.types";
-import React from "react";
+import { BRANDS_QUERYResult, Category, Product } from "@/sanity.types";
+import React, { useState } from "react";
 import Container from "./Container";
 import Title from "./Title";
 import CategoryList from "./shop/CategoryList";
 import BrandList from "./shop/BrandList";
+import { useSearchParams } from "next/navigation";
 interface Props {
   categories: Category[];
   brands: BRANDS_QUERYResult;
 }
 
 const Shop = ({ categories, brands }: Props) => {
+  const searchParams = useSearchParams();
+  const brandParams = searchParams?.get("brand");
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedBrand, setSelectedBrand] = useState<string | null>(
+    brandParams || null
+  );
+  const [selectedPrice, setSelectedPrice] = useState<string | null>(null);
+
   return (
     <div className="border-t">
       <Container className="mt-5">
@@ -27,7 +38,11 @@ const Shop = ({ categories, brands }: Props) => {
         <div className="flex flex-col md:flex-row gap-5 border-t border-t-shop_dark_green/50">
           <div className="md:sticky md:top-20 md:self-startmd:h-[calc(100vh-160px)] md:overflow-hidden pb-5 md:min-w-64 md:border-r border-r-shop_btn_dark_green/50">
             {/* ctegory list */}
-            <CategoryList />
+            <CategoryList
+              categories={categories}
+              selectedCategory={selectedCategory}
+              setSelectedCategory={setSelectedCategory}
+            />
             {/* brand list */}
             <BrandList />
             {/* price list */}
--- components/shop/CategoryList.tsx
+++ components/shop/CategoryList.tsx
@@ -1,7 +1,44 @@
+import { Category } from "@/sanity.types";
 import React from "react";
+import Title from "../Title";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Label } from "../ui/label";
+interface Props {
+  categories: Category[];
+  selectedCategory?: string | null;
+  setSelectedCategory: React.Dispatch<React.SetStateAction<string | null>>;
+}
 
-const CategoryList = () => {
-  return <div>CategoryList</div>;
+const CategoryList = ({
+  categories,
+  selectedCategory,
+  setSelectedCategory,
+}: Props) => {
+  return (
+    <div className="w-full bg-white p-5">
+      <Title className="text-base font-black">Product Categories</Title>
+      <RadioGroup>
+        {categories?.map((category) => (
+          <div
+            key={category?._id}
+            className="flex items-center space-x-2 hover:cursor-pointer"
+          >
+            <RadioGroupItem
+              value={category?.slug?.current as string}
+              id={category?.slug?.current}
+              className="rounded-sm"
+            />
+            <Label
+              htmlFor={category?.slug?.current}
+              className={`${selectedCategory}`}
+            >
+              {category?.title}
+            </Label>
+          </div>
+        ))}
+      </RadioGroup>
+    </div>
+  );
 };
 
 export default CategoryList;
