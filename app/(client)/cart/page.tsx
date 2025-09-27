4a63b30e8113dc1291b24b19f5b66e5886a3cd03
OGtrilliams <tribecca@tribecc.us>
2025-09-27 00:45:27 -0400

    cart summary page started

--- app/(client)/cart/page.tsx
+++ app/(client)/cart/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 import Container from "@/components/Container";
+import EmptyCart from "@/components/EmptyCart";
 import NoAccess from "@/components/NoAccess";
+import PriceFormatter from "@/components/PriceFormatter";
+import ProductSideMenu from "@/components/ProductSideMenu";
+import QuantityBtn from "@/components/QuantityBtn";
+import Title from "@/components/Title";
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { Address } from "@/sanity.types";
+import { urlFor } from "@/sanity/lib/image";
 import useStore from "@/store";
 import { useAuth, useUser } from "@clerk/nextjs";
+import { ShoppingBag, Trash } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const CartPage = () => {
   const {
@@ -15,17 +32,156 @@ const CartPage = () => {
     resetCart,
   } = useStore();
   const [loading, setLoading] = useState(false);
-  // const groupedItems = useStore((state) => state.getGroupedItems());
+  const groupedItems = useStore((state) => state.getGroupedItems());
   const { isSignedIn } = useAuth();
   const { user } = useUser();
   // const [addresses, setAddresses] = useState<Address[] | null>(null);
   const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
-
+  const [selectedPaymentMethod, setSelectedPaymentMethod] =
+    useState<string>("cash");
+  const handleResetCart = () => {
+    const confirmed = window.confirm(
+      "Are you sure you want to empty your cart?"
+    );
+    if (confirmed) {
+      resetCart();
+      toast.success("Cart emptied successfully.");
+    }
+  };
   return (
     <div className="bg-gray-50 pb-52 md:pb-10">
       {isSignedIn ? (
         <Container>
-          <p>Cart Page</p>
+          {groupedItems?.length ? (
+            <>
+              <div className="flex items-center gap2 py-5">
+                <ShoppingBag className="text-darkColor" />
+                <Title>Shopping Cart</Title>
+              </div>
+              <div className="grid lg:grid-cols-3 md:gap-8">
+                <div className="lg:col-span-2 rounded-lg">
+                  <div className="border bg-white rounded-md">
+                    {groupedItems?.map(({ product }) => {
+                      const itemCount = getItemCount(product?._id);
+                      return (
+                        <div
+                          key={product?._id}
+                          className="border-b p-2.5 last:border-b-0 flex items-center justify-between gap-5"
+                        >
+                          <div className="flex flex-1 items-start gap-2 h-36 md:h-44">
+                            {product?.images && (
+                              <Link
+                                href={`/product/${product?.slug?.current}`}
+                                className="border p-0.5 md:p-1 mr-2 rounded-md overflow-hidden group"
+                              >
+                                <Image
+                                  src={urlFor(product?.images[0]).url()}
+                                  alt="productImage"
+                                  width={500}
+                                  height={500}
+                                  loading="lazy"
+                                  className="w-32 md:w-40 md:h-40 object-cover group-hover:scale-105 hoverEffect"
+                                />
+                              </Link>
+                            )}
+                            <div className="h-full flex flex-1 flex-col justify-between py-1">
+                              <div className="flex flex-col gap-0.5 md:gap-1.5">
+                                <h2 className="text-base font-semibold line-clamp-1">
+                                  {product?.name}
+                                </h2>
+                                <p className="text-sm capitalize">
+                                  Variant:{" "}
+                                  <span className="font-semibold">
+                                    {product?.variant}
+                                  </span>
+                                </p>
+                                <p className="text-sm capitalize">
+                                  Status:{" "}
+                                  <span className="font-semibold">
+                                    {product?.status}
+                                  </span>
+                                </p>
+                              </div>
+
+                              <div className="flex items-center gap-2">
+                                <TooltipProvider>
+                                  <Tooltip>
+                                    <TooltipTrigger>
+                                      <ProductSideMenu
+                                        product={product}
+                                        className="relative top-0 right-0"
+                                      />
+                                      <TooltipContent className="font-bold">
+                                        Add to favorites
+                                      </TooltipContent>
+                                    </TooltipTrigger>
+                                  </Tooltip>
+                                  {/* delete item */}
+                                  <Tooltip>
+                                    <TooltipTrigger>
+                                      <Trash
+                                        onClick={() => {
+                                          (deleteCartProduct(product?._id),
+                                            toast.success(
+                                              "Product deleted successfully."
+                                            ));
+                                        }}
+                                        className="w-4 h-4 md:w-5 md:h-5 mr-1 text-gray-500 hover:text-red-600 hoverEffect"
+                                      />
+                                    </TooltipTrigger>
+                                    <TooltipContent className="font-bold bg-red-600">
+                                      Delete Product
+                                    </TooltipContent>
+                                  </Tooltip>
+                                </TooltipProvider>
+                              </div>
+                            </div>
+                            <div className="flex flex-col items-start justify-between h-36 md:h-34 p-0.5 md:p-1">
+                              <PriceFormatter
+                                amount={(product?.price as number) * itemCount}
+                                className="font-bold text-lg"
+                              />
+                              <QuantityBtn product={product} />
+                            </div>
+                          </div>
+                        </div>
+                      );
+                    })}
+                    <Button
+                      onClick={handleResetCart}
+                      className="m-5 font-semibold"
+                      variant="destructive"
+                    >
+                      Empty cart
+                    </Button>
+                  </div>
+                </div>
+                <div className="">
+                  <div className="lg:col-span-1">
+                    <div className="hidden md:inline-block w-full bg-white p-6 rounded-lg border">
+                      <h2 className="text-xl font-semibold mb-4">
+                        Order Summary
+                      </h2>
+                      <div className="space-y-4">
+                        <div className="">
+                          <span>Subtotal</span>{" "}
+                          <PriceFormatter amount={getSubTotalPrice()} />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* order summary fopr mobile view */}
+                <div className="md:hidden fixed bottom-0 left-0 w-full bg-white pt-2">
+                  <div className="bg-white p-4 rounded-lg border mx-4">
+                    <h2>Order Summary</h2>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <EmptyCart />
+          )}
         </Container>
       ) : (
         <NoAccess />
