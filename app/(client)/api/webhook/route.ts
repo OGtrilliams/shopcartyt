c523d5b59e9ea1bc28a8ceb42cd227424fb76c3b
OGtrilliams <tribecca@tribecc.us>
2025-09-29 11:13:33 -0400

    stripe routes

--- app/(client)/api/webhook/route.ts
+++ app/(client)/api/webhook/route.ts
@@ -1,1 +1,63 @@
 "use server";
+
+import stripe from "@/lib/stripe";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+export async function POST(req: NextRequest) {
+  const body = await req.text();
+  const headersList = await headers();
+  const sig = headersList.get("stripe-signature");
+
+  if (!sig) {
+    return NextResponse.json(
+      { error: "No signature found for stripe" },
+      { status: 400 }
+    );
+  }
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  if (!webhookSecret) {
+    console.log("Stripe webhook secret is not set");
+    return NextResponse.json(
+      {
+        error: "Stripe webhook secret is not set",
+      },
+      { status: 400 }
+    );
+  }
+  let event: Stripe.Event;
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (error) {
+    console.error("Webhook signature verification failed:", error);
+    return NextResponse.json(
+      {
+        error: `Webhook Error: ${error}`,
+      },
+      { status: 400 }
+    );
+  }
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
+    const invoice = session.invoice
+      ? await stripe.invoices.retrieve(session.invoice as string)
+      : null;
+
+    try {
+      await createOrderInSanity(session, invoice);
+    } catch (error) {
+      console.error("Error creating order in sanity:", error);
+      return NextResponse.json(
+        {
+          error: `Error creating order: ${error}`,
+        },
+        { status: 400 }
+      );
+    }
+  }
+}
+async function createOrderInSanity(
+  session: Stripe.Checkout.Session,
+  Invoice: Stripe.Invoice | null
+) {}
